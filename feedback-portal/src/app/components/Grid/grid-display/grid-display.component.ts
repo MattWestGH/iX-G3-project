2daa82a5593500fc949cb03b7d522e74f4d2af3c
Matt <workmailmattwest@gmail.com>
2022-01-11 14:27:48 +0200

    Added Grid items, formatted navbar, Made components and routes for different pages.

---
+++ feedback-portal/src/app/components/Grid/grid-display/grid-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-display',
+  templateUrl: './grid-display.component.html',
+  styleUrls: ['./grid-display.component.css']
+})
+export class GridDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
