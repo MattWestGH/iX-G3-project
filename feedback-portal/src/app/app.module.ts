34332754977ae48546ee8ad01af24cfa22dbca5e
Betsy Frekot <anesuatammandonga@gmailcom>
2022-01-12 13:23:05 +0200

    added modal to RATINGS with feedback panel

--- feedback-portal/src/app/app.module.ts
+++ feedback-portal/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    TasksComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ feedback-portal/src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  task: Task = new Task();
+  tasks: Task[] = [];
+
+  edit: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addTask() {
+    this.task.id = this.tasks.length;
+    this.tasks.push(this.task);
+    this.task = new Task();
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter((x) => x.id != task.id);
+  }
+
+  editTask(task: Task) {
+    this.edit = true;
+    this.task = task;
+  }
+
+  updateTask() {
+    this.edit = true;
+    this.tasks = this.tasks.filter((x) => x.id != this.task.id);
+    this.tasks.push(this.task);
+    this.task = new Task();
+    this.edit = false;
+  }
+
+}
